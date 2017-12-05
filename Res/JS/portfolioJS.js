83ea9a90fe406fbdb8ed4db46b6cdf3733610317
ScottishGuy95 <32342101+ScottishGuy95@users.noreply.github.com>
2017-12-05 09:47:12 +0000

    Prep to upload to GitPages
    I have updated the main html file, renamed and included more content. Responsive now across most devices( desktop, tablet, phone) and has different layouts per device. JavaScript files are now used to host all scripts and a new directory structure to host the files in a more sensible manner.

---
+++ Res/JS/portfolioJS.js
@@ -0,0 +1,68 @@
+function clickScroll( moveValue ) {
+  var screenW = window.innerWidth;
+  var distance = screenW * moveValue;
+  var divToMove = document.getElementById("main-container");
+  divToMove.scrollTo(0,0);
+  divToMove.scrollBy(0, distance);
+}
+
+function clickScrollPhone( moveValue ) {
+  var screenH = window.innerHeight;
+  var distance = screenH * moveValue;
+  var divToMove = document.getElementById("main-container");
+  divToMove.scrollTo(0,0);
+  divToMove.scrollBy(0, distance);
+}
+
+
+
+function displayImg1() {
+  var modal = document.getElementById("imgModal");
+  var modalImg = document.getElementById("imageArea");
+  var captionText = document.getElementById("caption");
+
+  modal.style.display = "block";
+  modalImg.src = "Res/Images/Portfoli_CouncilAppMobile.PNG";
+  modalImg.style.width = "250px";
+  modalImg.style.height = "450px";
+  captionText.innerHTML = "Council website";
+}
+
+function displayImg2() {
+  var modal = document.getElementById("imgModal");
+  var modalImg = document.getElementById("imageArea");
+  var captionText = document.getElementById("caption");
+
+  modal.style.display = "block";
+  modalImg.src = "Res/Images/Portfoli_JonJo.PNG";
+  captionText.innerHTML = "Pizza Delivery App";
+}
+
+function displayImg3() {
+  var modal = document.getElementById("imgModal");
+  var modalImg = document.getElementById("imageArea");
+  var captionText = document.getElementById("caption");
+
+  modal.style.display = "block";
+  modalImg.src = "Res/Images/Portfoli_Sokoban.PNG";
+  modalImg.style.width = "450px";
+  modalImg.style.height = "450px";
+  captionText.innerHTML = "Pizza Delivery App";
+}
+
+function displayImg4() {
+  var modal = document.getElementById("imgModal");
+  var modalImg = document.getElementById("imageArea");
+  var captionText = document.getElementById("caption");
+
+  modal.style.display = "block";
+  modalImg.src = "Res/Images/Portfoli_Stellar_Game.PNG";
+  modalImg.style.width = "450px";
+  modalImg.style.height = "350px";
+  captionText.innerHTML = "Pizza Delivery App";
+}
+
+function closeModal() {
+  var modal = document.getElementById("imgModal");
+  modal.style.display = "none";
+}
