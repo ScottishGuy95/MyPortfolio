7ea576f2c5687d69bd673c0012909ad511ceb134
ScottishGuy95 <32342101+ScottishGuy95@users.noreply.github.com>
2017-12-06 13:14:15 +0000

    Finished version

--- Res/JS/portfolioJS.js
+++ Res/JS/portfolioJS.js
@@ -1,68 +1,66 @@
-function clickScroll( moveValue ) {
-  var screenW = window.innerWidth;
-  var distance = screenW * moveValue;
-  var divToMove = document.getElementById("main-container");
-  divToMove.scrollBy(0,0);
-  divToMove.scrollBy(0, distance);
+function clickScroll( moveValue ) {                           //This function will move the windows screen to the specified section
+  var screenW = window.innerWidth;                            //Obtain the screen width (viewport width)
+  var distance = screenW * moveValue;                         //Multiply 100vw by given value
+  var divToMove = document.getElementById("main-container");  //Select the div to move
+  divToMove.scroll(0,0);                                      //Reset the window position to far left
+  divToMove.scrollBy(0, distance);                            //Move the scrollbar to the new position
 }
 
-function clickScrollPhone( moveValue ) {
-  var screenH = window.innerHeight;
-  var distance = screenH * moveValue;
-  var divToMove = document.getElementById("main-container");
-  divToMove.scrollTo(0,0);
-  divToMove.scrollBy(0, distance);
+function clickScrollPhone( moveValue ) {                      //This function will move the windows screen to the specified section
+  var screenH = window.innerHeight;                           //Obtain the screen height (viewport height)
+  var distance = screenH * moveValue;                         //Multiply 100vh by given value
+  var divToMove = document.getElementById("main-container");  //Select the div to move
+  divToMove.scroll(0,0);                                      //Reset the window position to top
+  divToMove.scrollBy(0, distance);                            //Move the scrollbar to the new position
 }
 
+function displayImg1() {                                //Function to display a specified image in a modal box
+  var modal = document.getElementById("imgModal");      //Get the modal box div
+  var modalImg = document.getElementById("imageArea");  //Get the modal div to display the image
+  var captionText = document.getElementById("caption"); //Get the text to store under the image
 
-
-function displayImg1() {
-  var modal = document.getElementById("imgModal");
-  var modalImg = document.getElementById("imageArea");
-  var captionText = document.getElementById("caption");
-
-  modal.style.display = "block";
-  modalImg.src = "Res/Images/Portfoli_CouncilAppMobile.PNG";
-  modalImg.style.width = "250px";
-  modalImg.style.height = "450px";
-  captionText.innerHTML = "Council website";
+  modal.style.display = "block";                              //Set the modal to be shown
+  modalImg.src = "Res/Images/Portfoli_CouncilAppMobile.PNG";  //The source of the image
+  modalImg.style.width = "250px";                             //The width of the image
+  modalImg.style.height = "450px";                            //The height of the image
+  captionText.innerHTML = "Council website";                  //The caption for the image
 }
 
-function displayImg2() {
-  var modal = document.getElementById("imgModal");
-  var modalImg = document.getElementById("imageArea");
-  var captionText = document.getElementById("caption");
+function displayImg2() {                                //Function to display a specified image in a modal box
+  var modal = document.getElementById("imgModal");      //Get the modal box div
+  var modalImg = document.getElementById("imageArea");  //Get the modal div to display the image
+  var captionText = document.getElementById("caption"); //Get the text to store under the image
 
-  modal.style.display = "block";
-  modalImg.src = "Res/Images/Portfoli_JonJo.PNG";
-  captionText.innerHTML = "Pizza Delivery App";
+  modal.style.display = "block";                        //Set the modal to be shown
+  modalImg.src = "Res/Images/Portfoli_JonJo.PNG";       //The source of the image
+  captionText.innerHTML = "Pizza Delivery App";         //The caption for the image
 }
 
-function displayImg3() {
-  var modal = document.getElementById("imgModal");
-  var modalImg = document.getElementById("imageArea");
-  var captionText = document.getElementById("caption");
+function displayImg3() {                                //Function to display a specified image in a modal box
+  var modal = document.getElementById("imgModal");      //Get the modal box div
+  var modalImg = document.getElementById("imageArea");  //Get the modal div to display the image
+  var captionText = document.getElementById("caption"); //Get the text to store under the image
 
-  modal.style.display = "block";
-  modalImg.src = "Res/Images/Portfoli_Sokoban.PNG";
-  modalImg.style.width = "450px";
-  modalImg.style.height = "450px";
-  captionText.innerHTML = "Pizza Delivery App";
+  modal.style.display = "block";                        //Set the modal to be shown
+  modalImg.src = "Res/Images/Portfoli_Sokoban.PNG";     //The source of the image
+  modalImg.style.width = "450px";                       //The width of the image
+  modalImg.style.height = "450px";                      //The height of the image
+  captionText.innerHTML = "Pizza Delivery App";         //The caption for the image
 }
 
-function displayImg4() {
-  var modal = document.getElementById("imgModal");
-  var modalImg = document.getElementById("imageArea");
-  var captionText = document.getElementById("caption");
+function displayImg4() {                                //Function to display a specified image in a modal box
+  var modal = document.getElementById("imgModal");      //Get the modal box div
+  var modalImg = document.getElementById("imageArea");  //Get the modal div to display the image
+  var captionText = document.getElementById("caption"); //Get the text to store under the image
 
-  modal.style.display = "block";
-  modalImg.src = "Res/Images/Portfoli_Stellar_Game.PNG";
-  modalImg.style.width = "450px";
-  modalImg.style.height = "350px";
-  captionText.innerHTML = "Pizza Delivery App";
+  modal.style.display = "block";                              //Set the modal to be shown
+  modalImg.src = "Res/Images/Portfoli_Stellar_Game.PNG";      //The source of the image
+  modalImg.style.width = "450px";                             //The width of the image
+  modalImg.style.height = "350px";                            //The height of the image
+  captionText.innerHTML = "Pizza Delivery App";               //The caption for the image
 }
 
 function closeModal() {
-  var modal = document.getElementById("imgModal");
-  modal.style.display = "none";
+  var modal = document.getElementById("imgModal");       //Get the modal box div
+  modal.style.display = "none";                          //Set the modal to be hidden
 }
