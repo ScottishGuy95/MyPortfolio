8816988e547933366ee92e0ceef9f94190faafb6
ScottishGuy95 <32342101+ScottishGuy95@users.noreply.github.com>
2019-01-27 23:20:58 +0000

    Add files via upload

--- Res/JS/portfolioJS.js
+++ Res/JS/portfolioJS.js
@@ -72,3 +72,37 @@ function closeModal() {
   var modal = document.getElementById("imgModal");            //Get the modal box div
   modal.style.display = "none";                               //Set the modal to be hidden
 }
+
+//Slideshow
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide() {
+  //showSlides(slideIndex = n);
+  showSlides( slideIndex );
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+
+  if (n > slides.length) {
+    slideIndex = 1
+  }
+
+  if (n < 1) {
+    slideIndex = slides.length
+  }
+
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  slides[slideIndex - 1].style.display = "block";
+}
+
+
+//Typewriter Effect
